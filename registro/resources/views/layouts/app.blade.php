d2d61ba971f778111432fec79036767a90553510
Kizzesss <73948212+Kizzesss@users.noreply.github.com>
2021-10-16 10:23:00 -0500

    Soldados solo con auth barra arriba

--- registro/resources/views/layouts/app.blade.php
+++ registro/resources/views/layouts/app.blade.php
@@ -33,14 +33,15 @@
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
+                
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    @if(Auth::check())
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item"></li>
                         <a class="nav-link" href="{{ route('soldado.index') }}">{{ __('Soldados') }}</a>
                     </ul>
-
+                    @endif
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
